5a9bffc1e1a1603280092319616380d7cfcc76f9
bipesh369 <juniorbipesh@gmail.com>
2026-06-16 17:48:11 +0000

    learn about types

---
+++ typescript-demo/src/typesInTs.ts
@@ -0,0 +1,19 @@
+let drink = "water";
+
+let cups = Math.random() > 0.5 ? 10 : 5;
+
+let chanceName = "Chaiaurcode"
+
+// type inference: means typescript authomatically figure out the type for you
+let age = 22;
+//typescript sees 22 and infers
+//let age:number;
+
+
+// type annotation: means you explicitly tell TypeScript what the type is.
+let chaiFlavour:string = "masala chai"
+chaiFlavour = "Ginger tea"
+
+let chaiOrder:number = 10
+
+let chaiGet:boolean = true
