86aac54ca33601c64a7cb78ad9aa5c62c3d4fb28
bipesh369 <juniorbipesh@gmail.com>
2026-06-16 17:04:10 +0000

    Initial commit

---
+++ typescript-demo/index.js
@@ -0,0 +1,6 @@
+"use strict";
+// index.ts
+Object.defineProperty(exports, "__esModule", { value: true });
+const message = "Hello TypeScript";
+console.log(message);
+//# sourceMappingURL=index.js.map
---
+++ typescript-demo/index.ts
@@ -0,0 +1,5 @@
+// index.ts
+
+const message: string = "Hello TypeScript";
+
+console.log(message);
