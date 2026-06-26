cf4114f9828877ddd94d79585988f6b749366b1b
bipesh369 <juniorbipesh@gmail.com>
2026-06-26 10:03:04 +0000

    learn about arrayTs

---
+++ typescript-demo/src/arrayTs.ts
@@ -0,0 +1,23 @@
+const chaiFlavour: string[] = ["Masala", "Adrak"]
+const chaiPrice: number[] = [10, 20]
+
+const rating: Array<number> = [4.5, 4.6]
+
+type Chai = {
+  name: string
+  price: number
+}
+
+const menu: Chai[] = [
+  {name: "Masala", price: 15},
+  {name: "Adrak", price: 25},
+]
+
+
+const cities: readonly string[] = ["KTM", "NPJL"]
+
+
+const table: number[] [] = [
+  [1, 2, 3],
+  [4, 5, 6]
+]
