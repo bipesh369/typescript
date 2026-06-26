15ea044c3cd3f046fe5f9f2a2bd19cc29697cca1
bipesh369 <juniorbipesh@gmail.com>
2026-06-26 10:11:00 +0000

    learn about tupleTs

---
+++ typescript-demo/src/tupleTs.ts
@@ -0,0 +1,14 @@
+let chaiTuple: [string, number]
+chaiTuple = ["Masala", 20]
+chaiTuple = ["Adrak", 25]
+
+
+let userInfo: [string, number, boolean?]
+userInfo = ["Bipesh", 100, true]
+
+// can not change the value of readonly datatype
+const location: readonly [number, number] = [28.5, 32.5]
+
+
+// name tuple
+const chaiItems: [name: string, price: number] = ["Masala", 23]
