7a915a27b6cdbdee4fbf7994c17a9730aae698c2
bipesh369 <juniorbipesh@gmail.com>
2026-06-26 05:02:44 +0000

    learn about function type

---
+++ typescript-demo/src/functionTs.ts
@@ -0,0 +1,23 @@
+function makeChai(type: string, cups: number) {
+  console.log(`making ${cups} cups of ${type}`)
+}
+
+
+makeChai("masala", 5)
+function getChaiPrice(): number {
+  return 20
+}
+
+function makeOrder(order: string){
+  if(!order) return null
+  return order
+}
+
+
+function createChai(ordr: {
+  type: string
+  sugar: number
+  size: "small" | "large"
+}): number{
+  return 4
+}
