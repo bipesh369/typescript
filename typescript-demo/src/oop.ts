cd97394dab9b5b168ed8a0f7c1ee501feff8fc1b
bipesh369 <juniorbipesh@gmail.com>
2026-06-27 09:05:06 +0000

    learn about access modifier

--- typescript-demo/src/oop.ts
+++ typescript-demo/src/oop.ts
@@ -16,3 +16,47 @@
 
 // const masalaChai = new Chai("Ginger")
 // masalaChai.flavour = "masala"
+
+
+
+// Access Modifier
+class Chai {
+  public flavour: string = "Masala"
+  private secretIngredients = "Cardamom"
+
+  reveal(){
+     return this.secretIngredients   //ok
+  }
+
+}
+
+class Shop {
+  protected shopName = "Chai Flavour"
+}
+class Branch extends Shop {
+  getName(){
+    return this.shopName  //ok
+  }
+}
+
+new Branch().getName
+
+//const c = new Chai()
+
+
+
+
+class ModernChai {
+  private _sugar = 2
+
+  get sugar(){
+    return this._sugar
+  }
+
+  set sugar(value: number){
+    this._sugar = value
+  }
+}
+
+const c = new ModernChai()
+c.sugar = 5
