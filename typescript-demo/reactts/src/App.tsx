0b488e1beb1c2676a0253cab290225596083d7e5
bipesh369 <juniorbipesh@gmail.com>
2026-06-28 18:14:47 +0000

    make counter component

--- typescript-demo/reactts/src/App.tsx
+++ typescript-demo/reactts/src/App.tsx
@@ -1,15 +1,22 @@
 import './App.css'
 import { ChaiCard } from './components/ChaiCard'
+import { Counter } from './components/Counter'
 
 function App() {
 
   return (
     <>
-      <div>Starting React with Typescript</div>
-
-      <ChaiCard name = "Laptop" price = {85000} />
+      <div>
+        <h1>Starting React with Typescript</h1>
+        <ChaiCard name = "Laptop" price = {85000} />
       <ChaiCard name = "Mobile" price = {30000} />
-    </>
+      </div>
+
+      <div>
+        <Counter />
+      </div>
+
+     </>
   )
 }
 
