8555ffe2c64f44df88e4191f47f8dd6838c08847
bipesh369 <juniorbipesh@gmail.com>
2026-06-29 09:42:37 +0000

    make types, ChaiList, ChaiCard

--- typescript-demo/reactts/src/App.tsx
+++ typescript-demo/reactts/src/App.tsx
@@ -1,6 +1,15 @@
 import './App.css'
 import { ChaiCard } from './components/ChaiCard'
+import { ChaiList } from './components/ChaiList.tsx'
 import { Counter } from './components/Counter'
+ import type { Chai } from "./types.ts"
+
+
+ const menu: Chai[] = [
+  {id: 1, name: "Masala", price: 30},
+  {id: 2, name: "Ginger", price: 50},
+  {id: 1, name: "Lemon", price: 60},
+ ]
 
 function App() {
 
@@ -14,6 +23,9 @@ function App() {
 
       <div>
         <Counter />
+      </div>
+      <div>
+        <ChaiList items={menu} />
       </div>
 
      </>
